46f293cb3de4dd011b1e3e1d0000430916bba8e1
Dayane <acdayane@gmail.com>
2023-11-23 08:22:14 -0300

    feat/cria util invoice calculator

---
+++ src/main/java/br/ada/invoiceService/utils/InvoiceCalculator.java
@@ -0,0 +1,11 @@
+package br.ada.invoiceService.utils;
+
+import java.math.BigDecimal;
+
+public class InvoiceCalculator {
+    
+    public static BigDecimal calculateInvoiceValue (BigDecimal totalPackages, BigDecimal totalDiscount) {        
+        BigDecimal value = totalPackages.subtract(totalDiscount);
+        return value.max(BigDecimal.ZERO);
+    }
+}
