63660cad56155dba9de7ca3cf8b4cf7dffc35d13
tdthais <thais.tdourado@gmail.com>
2023-11-24 15:39:34 -0300

    feat/ajustes-filas

--- src/main/java/br/ada/invoiceService/controller/TestController.java
+++ src/main/java/br/ada/invoiceService/controller/TestController.java
@@ -1,9 +1,10 @@
-package br.ada.invoiceService.queue.controller;
+package br.ada.invoiceService.controller;
 
 
 import br.ada.invoiceService.queue.QueueProducerPayment;
 import br.ada.invoiceService.service.InvoiceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,16 +12,15 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @RestController
-@RequestMapping("/test")
+//@RequestMapping("/test")
 public class TestController {
+    @Autowired
+    InvoiceService invoiceService;
 
-    private InvoiceService invoiceService;
-
-
+    @GetMapping("/health-check")
     public String testingProducer(){
         invoiceService.issueInvoice2Payment("invoice1", "user1", LocalDate.of(2023,11,23), BigDecimal.valueOf(200));
         return "ok. done";
     }
-
 
 }
